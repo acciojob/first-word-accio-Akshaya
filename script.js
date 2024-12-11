d58a560db215aa19813961bdc488fcae587792bc
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-11 08:22:05 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	const spaceIndex = s.indexOf(' ');
+	
+    return spaceIndex === -1 ? s : s.substring(0, spaceIndex);
+	
 }
+console.log(firstWord('see and stop'));	
+console.log(firstWord(' Hello World!'));
+console.log(firstWord('12345'));	
+console.log(firstWord(''));
 
 // Do not change the code below
 
